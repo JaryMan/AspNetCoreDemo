432eb4e1cab73e4a1d3ca790a0cbd96361a82a2b
Administrator <Administrator@ALEX>
2016-07-07 11:24:37 +0800

    1111

---
+++ src/AspNetCore.Demo.Enterprise/DateBase/Data/IDataClass/IUserData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Demo.Enterprise.DateBase.Data.IDataClass
+{
+    public class IUserData
+    {
+    }
+}
---
+++ src/AspNetCore.Demo.Enterprise/DateBase/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Demo.Enterprise.DateBase.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
