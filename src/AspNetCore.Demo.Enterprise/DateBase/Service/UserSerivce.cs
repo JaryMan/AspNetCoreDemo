490a9e90357a5ebe3a45c7fe6a2354e1f25035e4
Administrator <Administrator@ALEX>
2016-07-21 10:32:35 +0800

    11111

--- src/AspNetCore.Demo.Enterprise/DateBase/Service/UserSerivce.cs
+++ src/AspNetCore.Demo.Enterprise/DateBase/Service/UserSerivce.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AspNetCore.Demo.Enterprise.DateBase.Service
 {
-    public class UserSerivce: SerivceBase<User>
+    public class UserSerivce
     {
     }
 }
